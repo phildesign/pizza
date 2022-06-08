82f8435cb6b33c0296be8beda26fa6dfe9351b0a
Phil <phil.nice.23@gmail.com>
2022-06-08 20:53:04 +0300

    add category state

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Categories = () => {
+	const categoties = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+	const [activeCategory, setActiveCategory] = useState(0);
+
+	const onClickCategory = (index) => {
+		setActiveCategory(index);
+	};
+
 	return (
 		<div className="categories">
 			<ul>
-				<li className="active">Все</li>
-				<li>Мясные</li>
-				<li>Вегетарианская</li>
-				<li>Гриль</li>
-				<li>Острые</li>
-				<li>Закрытые</li>
+				{categoties.map((item, index) => {
+					return (
+						<li
+							className={activeCategory === index ? 'active' : ''}
+							onClick={() => onClickCategory(index)}
+							key={item + index}>
+							{item}
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);

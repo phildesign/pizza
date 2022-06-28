2fcc922d5f140c4bdbfcf6d85c43a08edde23ea8
Phil <phil.nice.23@gmail.com>
2022-06-28 17:21:31 +0300

    add fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
 import Categories from './components/Categories/Categories';
 import Header from './components/Header/Header';
 import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 import Sort from './components/Sort/Sort';
-import pizzas from './assets/pizzas.json';
 
 import './scss/app.scss';
 
 const App = () => {
+	const [pizzas, setPizzas] = useState([]);
+
+	useEffect(() => {
+		fetch('https://62bb03dc573ca8f83290dec6.mockapi.io/items')
+			.then((res) => res.json())
+			.then((data) => {
+				setPizzas(data);
+			});
+	}, []);
+
 	return (
 		<div className="wrapper">
 			<Header />
